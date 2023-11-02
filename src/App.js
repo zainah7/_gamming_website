ee803bf54d2d381a0c07d8f87e49c71ece77f41f
zainah7 <zainaabd7@gmail.com>
2023-11-02 10:51:54 +0100

    gamming website

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import COD from "./components/COD";
+import Footer from "./components/Footer";
+import GameCenters from "./components/GameCenters";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Trending from "./components/Trending";
+
+function App() {
+  return (
+    <main className="text-white">
+      <Header />
+
+      <div className="w-full xl:max-w-[1250px] mx-auto px-4 ">
+        <Hero />
+        <Trending />
+        <COD />
+        <GameCenters />
+      </div>
+
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
